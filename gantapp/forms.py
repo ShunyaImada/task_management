e9852551d699d77fe5723b66591ed1a46d1d95d9
shunya <imada@example.com>
2022-05-29 08:48:16 +0900

    1st

--- gantapp/forms.py
+++ gantapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.admin.widgets import AdminDateWidget    #インポート
 from .models import Tasks,Projects
+import bootstrap_datepicker_plus as bootstrap_datepicker
 
 
  
@@ -19,7 +20,7 @@ class AddTaskForm(forms.ModelForm):
         super(AddTaskForm, self).__init__(*args, **kwargs)
         print(user)
         self.fields['project'].queryset = Projects.objects.filter(owner=user)
-        self.fields['taskName'].queryset = Tasks.objects.filter(owner=user)
+        #self.fields['taskName'].queryset = Tasks.objects.filter(owner=user)
     
         
 class AddProjectForm(forms.ModelForm):
